7e18eb902f89af992cb9ed54a0057de42c33dbf8
Fernandosilvasc <fernando_silvasc@hotmail.com>
2023-02-04 16:49:41 -0800

    created components Header and Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import {Header} from './components/Header'
+import {Post} from './components/Post'
 import './global.css'
+import styles from './App.module.css'
+import {Sidebar} from './components/Sidebar'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import styles from './Sidebar.module.css';
+import {PencilLine} from "phosphor-react";
+
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://tinyurl.com/4b8yhscc" />
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/58282921?v=4" alt="Avatar - Fernando Silva"
+        />
+        <strong>Fernando Correa da Silva</strong>
+        <span>FullStack Engineer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Edit your profile
+        </a>
+      </footer>
+    </aside>
+  )
+}
